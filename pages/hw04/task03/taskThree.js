5be7673987a4b464224b6a89d258458f503c7311
Sashko <sashkoace@gmail.com>
2020-09-02 08:56:26 +0300

    apdated after consultation

--- pages/hw04/task03/taskThree.js
+++ pages/hw04/task03/taskThree.js
@@ -80,8 +80,6 @@ concat(arr, arr2);
 function concat(first, second) {
     first.forEach(item => {
         newArr.splice(newArr.length, 0, item);
-    });
-    second.forEach(item => {
         newArr.splice(newArr.length, 0, item);
     });
 }
--- pages/hw04/task04/taskFour.js
+++ pages/hw04/task04/taskFour.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const color = document.querySelectorAll('td'),
-    perentColors = document.querySelector('table'),
-    bg = document.querySelector('.taskFour');
+const perentWrapper = document.querySelector('table'),
+    bg = document.querySelector('.taskFour'),
+    clases = ['one1', 'one2', 'one3', 'one3', 'two1', 'two2', 'two3', 'two4', 'three1', 'three2', 'three3', 'three4', 'five1', 'five2', 'five3', 'five4', 'six1', 'six2', 'six3', 'six4', 'seven1', 'seven2', 'seven3', 'seven4'];
 
-perentColors.addEventListener('click', (e) => {
+perentWrapper.addEventListener('click', (e) => {
     if (e.target && e.target.tagName == 'TD') {
         const bgColor = e.toElement.className;
 
-        bg.classList.remove(bgColor);
+        bg.classList.remove(...clases);
         bg.classList.add(bgColor);
 
         console.log(bg)
